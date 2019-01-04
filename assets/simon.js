3758297c531a4f505a5f44c812f9b152386ab098
Benjamin Orton <b.r.orton@gmail.com>
2019-01-04 11:57:45 +0000

    computer turn colours flashing and clearColours added

--- assets/simon.js
+++ assets/simon.js
@@ -1,14 +1,28 @@
 /*create simon object*/
 $(document).ready(function() {
     let simonObject = {
-        gameOrder: [2, 5, 5],
-        playerOrder: [7, 5, 6],
+        playerOrder: [],
+        gameOrder: [],
+        currentTurn: 0,
+        computerColorCount: 0,
         playerTurn: false,
-        numberOfColours: 4
+        numberOfColours: 4,
+        colours: [red, green, blue, yellow]
     };
-    function clearColours(){
-        /*remove any lights on colours*/
+    /*computer turn functions*/
+    function computerFlashTimeout() {
+        console.log(simonObject.gameOrder[simonObject.computerColorCount]);
+        var thisFlash = simonObject.gameOrder[simonObject.computerColorCount];
+        var thisColour = simonObject.colours[thisFlash - 1];
+        console.log(thisColour);
+        $(thisColour).addClass("light-up");
+        simonObject.computerColorCount++;
     }
+
+    function clearColours() {
+        $(".simon-color").removeClass("light-up");
+    }
+    /*end of computer turn functions*/
 
     /*startgame
     clear all of old games 
@@ -18,12 +32,14 @@ $(document).ready(function() {
     $("#play").click(function() {
         clearColours();
         simonObject.gameOrder = [];
-        simonObject.playerOrder=[];
-        playerTurn=false;
-        for (var i=0; i<20; i++){
-            simonObject.gameOrder.push(Math.floor(Math.random()*simonObject.numberOfColours)+1);
+        simonObject.playerOrder = [];
+        simonObject.currentTurn = 20;
+        simonObject.playerTurn = false;
+        for (var i = 0; i < 20; i++) {
+            simonObject.gameOrder.push(Math.floor(Math.random() * simonObject.numberOfColours) + 1);
         }
         console.log(simonObject);
+        computerTurn();
     });
 
 
@@ -33,7 +49,36 @@ $(document).ready(function() {
     flash correct amount of colours from gameOrder 
     start player turn
     */
+    function computerTurn() {
+        simonObject.playerTurn = false;
+        simonObject.computerColorCount = 0;
+        var lightUpInterval = setInterval(lightUp, 500);
 
+        function lightUp() {
+            if (simonObject.computerColorCount < simonObject.currentTurn) {
+                clearColours();
+                setTimeout(computerFlashTimeout, 100);
+            }
+            else {
+                clearColours();
+                clearInterval(lightUpInterval);
+            }
+        }
+        simonObject.currentTurn++;
+        playerTurn();
+    }
+
+    function playerTurn() {
+        clearColours();
+        /*
+        if (simonObject.currentTurn > 20) {
+           console.log("end");
+        }else{
+             simonObject.currentTurn++;
+             computerTurn();
+        }
+        */
+    }
     /*user turn
     allow player to click.
     store player order and check against gameOrder
